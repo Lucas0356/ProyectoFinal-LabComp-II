18f1f8ede860c36f8ccfb7d6d1dbd1006ed9f87e
lucas moltedo <lucasmoltedo03@gmail.com>
2023-02-03 19:44:46 -0300

    Se añadió seguridad en perfil.html, se cambiaron los efectos en javascript

--- js/perfil.js
+++ js/perfil.js
@@ -4,34 +4,70 @@ Autores: Lucas Moltedo - Martín Reser
 
 ***********************************************/
 
-const editarButton = document.querySelector('#boton-editar');
+// INFORMACION PERSONAL:
+
+const editarBotonPersonal = document.querySelector('#boton-editar-personal');
 
 // querySelector nos permite seleccionar un elemento
 // Lo que encuentre se guardará en una const (constante) llamada editarButton
 
 // Agregamos un evento al elemento para cuando se le haga click:
-editarButton.addEventListener('click', function () {
+editarBotonPersonal.addEventListener('click', function () {
     // Con "getElementById" seleccionamos un elemento por un ID:
     document.getElementById('editor-info-personal').classList.toggle('active') // info personal editar
-    document.getElementById('editor-info-privacidad').classList.toggle('active') // info privacidad editar
-    document.getElementById('info-personal').classList.toggle('active') //Acá ocultamos la informacion sin editar
+    document.getElementById('info-seguridad').classList.toggle('active') //Acá ocultamos la informacion sin editar
     // Con ".classList.toggle()" le podemos agregar/quitar una clase (depende si la tiene), a un elemento
     // Con esto ya tenemos la función de mostrar/ocultar los editores de información
+
 })
 
 // Botón para cancelar la edición de la información
 
-const botoncerrar = document.querySelector('.boton-cerrar');
+const botoncerrarPersonal = document.querySelector('.boton-cerrar-personal');
 
 // querySelector nos permite seleccionar un elemento
 // Lo que encuentre se guardará en una const (constante) llamada botoncerrar
 
 // Agregamos un evento al elemento para cuando se le haga click:
-botoncerrar.addEventListener('click', function () {
+botoncerrarPersonal.addEventListener('click', function () {
     // Con "getElementById" seleccionamos un elemento por un ID:
     document.getElementById('editor-info-personal').classList.toggle('active')
-    document.getElementById('editor-info-privacidad').classList.toggle('active')
-    document.getElementById('info-personal').classList.toggle('active') 
+    document.getElementById('info-seguridad').classList.toggle('active')
+    // Con ".classList.toggle()" le podemos agregar/quitar una clase (depende si la tiene), a un elemento
+    // Con esto ya tenemos la función de ocultar los editores de información y volver a mostrar
+    // la informacion personal
+})
+
+// ######################################################################################################################
+// SEGURIDAD:
+
+const editarBotonSeguridad = document.querySelector('#boton-editar-seguridad');
+
+// querySelector nos permite seleccionar un elemento
+// Lo que encuentre se guardará en una const (constante) llamada editarButton
+
+// Agregamos un evento al elemento para cuando se le haga click:
+editarBotonSeguridad.addEventListener('click', function () {
+    // Con "getElementById" seleccionamos un elemento por un ID:
+    document.getElementById('editor-info-seguridad').classList.toggle('active') // info personal editar
+    document.getElementById('info-personal').classList.toggle('active') //Acá ocultamos la informacion sin editar
+    // Con ".classList.toggle()" le podemos agregar/quitar una clase (depende si la tiene), a un elemento
+    // Con esto ya tenemos la función de mostrar/ocultar los editores de información
+
+})
+
+// Botón para cancelar la edición de la información
+
+const botonCerrarSeguridad = document.querySelector('.boton-cerrar-seguridad');
+
+// querySelector nos permite seleccionar un elemento
+// Lo que encuentre se guardará en una const (constante) llamada botoncerrar
+
+// Agregamos un evento al elemento para cuando se le haga click:
+botonCerrarSeguridad.addEventListener('click', function () {
+    // Con "getElementById" seleccionamos un elemento por un ID:
+    document.getElementById('editor-info-seguridad').classList.toggle('active')
+    document.getElementById('info-personal').classList.toggle('active')
     // Con ".classList.toggle()" le podemos agregar/quitar una clase (depende si la tiene), a un elemento
     // Con esto ya tenemos la función de ocultar los editores de información y volver a mostrar
     // la informacion personal
